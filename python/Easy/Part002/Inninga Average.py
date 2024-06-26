0bb39d4c8b4f2d5dee550a0ed34ced965e0e6528
HAJITH-M <118360914+HAJITH-M@users.noreply.github.com>
2024-06-26 23:53:52 +0530

    Update Inninga Average.py

--- python/Easy/Part002/Inninga Average.py
+++ python/Easy/Part002/Inninga Average.py
@@ -1,22 +1,22 @@
 def main():
     
-        n = int(input().strip())
+    n = int(input().strip())
 
-            sum_first_innings = 0
-                sum_second_innings = 0
-                    
-                        for _ in range(n):
-                                line = input().strip()
-                                        first_innings, second_innings = map(int, line.split())
-                                                
-                                                        sum_first_innings += first_innings
-                                                                sum_second_innings += second_innings
-                                                                    
-                                                                        avg_first_innings = sum_first_innings / n
-                                                                            avg_second_innings = sum_second_innings / n
-                                                                                
-                                                                                    print(f"{avg_first_innings:.2f}")
-                                                                                        print(f"{avg_second_innings:.2f}")
+    sum_first_innings = 0
+    sum_second_innings = 0
+    
+    for _ in range(n):
+        line = input().strip()
+        first_innings, second_innings = map(int, line.split())
+        
+        sum_first_innings += first_innings
+        sum_second_innings += second_innings
+    
+    avg_first_innings = sum_first_innings / n
+    avg_second_innings = sum_second_innings / n
+    
+    print(f"{avg_first_innings:.2f}")
+    print(f"{avg_second_innings:.2f}")
 
-                                                                                        if __name__ == "__main__":
-                                                                                            main()
+if __name__ == "__main__":
+    main()
